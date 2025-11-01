34c5bfe1df32e7852216831455edc8f111863591
sam dien <sxyn929@gmail.com>
2025-11-01 08:02:55 +0000

    still not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { Header } from '@/components/firewall-insights/header';
+import { MainContent } from '@/components/firewall-insights/main-content';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
+      <Header />
       <main className="flex-1 container mx-auto p-4 sm:p-6 lg:p-8">
-        <h1 className="text-4xl font-bold">Hello World!</h1>
-        <p>If you see this, the basic app is working.</p>
+        <MainContent />
       </main>
     </div>
   );
