682d2b536cfddc954480db3bec41d5e9ec957d08
sam dien <sxyn929@gmail.com>
2025-11-01 08:01:56 +0000

    how to run nothing show

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { Header } from '@/components/firewall-insights/header';
-import { MainContent } from '@/components/firewall-insights/main-content';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
-      <Header />
       <main className="flex-1 container mx-auto p-4 sm:p-6 lg:p-8">
-        <MainContent />
+        <h1 className="text-4xl font-bold">Hello World!</h1>
+        <p>If you see this, the basic app is working.</p>
       </main>
     </div>
   );
